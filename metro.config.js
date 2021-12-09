686dbdfda5006b7640081462c7983657b57dd13f
Sourab <shubpramanik241@gmail.com>
2021-12-09 20:40:36 +0530

    Added Icon, Adaptive Icon, Splash Screen

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  resolver: {
+    blacklistRE: /#current-cloud-backend\/.*/  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+};
